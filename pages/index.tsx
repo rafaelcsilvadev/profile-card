d94f37a55f987675a43326c580cc13ef43d14eee
Rafael Couto <rafaelcsilva016@outlook.com>
2021-02-18 13:19:56 -0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import style from '../sass/main.module.scss';
+
+function Home() {
+  return (
+    <div>
+      <button className={style.button}>Clique</button>
+      <span className={style.font}>OIII</span>
+      <a className={style.link}>OIII</a>
+    </div>
+  );
+}
+
+export default Home;
