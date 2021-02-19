8eaa5a3d86bedaa2693300140e027418067dea76
Rafael Couto <rafaelcsilva016@outlook.com>
2021-02-19 17:12:11 -0300

    create home page

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -9,15 +9,36 @@ function Layout({ children }) {
       </header>
       <main>{children}</main>
       <footer className={style.footer}>
-          <span>
-              © Rafael Couto Silva - 
-          </span>
-          <span>
-              (24) 9 9964-4346 - 
-          </span>
-          <span>
-               rafaelcsilva016@outlook.com
-          </span>
+        <div className={style.author}>
+          <span>© Rafael Couto Silva</span>
+          <a
+            href="https://api.whatsapp.com/send?phone=+5524999644346"
+            className={style.link}
+          >
+            (24) 9 9964-4346
+          </a>
+          <a href="mailto:rafaelcsilva016@outlook.com" className={style.link}>
+            rafaelcsilva016@outlook.com
+          </a>
+        </div>
+        <div className={style.icons}>
+          Icons made by{" "}
+          <a
+            href="https://www.freepik.com"
+            title="Freepik"
+            className={style.link}
+          >
+            Freepik
+          </a>{" "}
+          from{" "}
+          <a
+            href="https://www.flaticon.com/"
+            title="Flaticon"
+            className={style.link}
+          >
+            www.flaticon.com
+          </a>
+        </div>
       </footer>
     </div>
   );
