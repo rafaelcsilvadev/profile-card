c0ae4bd65277585fd50be56b6a64ce1affdabd3e
Rafael Couto <rafaelcsilva016@outlook.com>
2021-02-22 16:07:50 -0300

    swr initialized

--- components/form/index.tsx
+++ components/form/index.tsx
@@ -1,13 +1,22 @@
+import { FormEvent, useState, useEffect } from "react";
 import style from "../../sass/main.module.scss";
 
 function Form() {
+  const [user, setUser] = useState('');
+
+  const handleShowUser = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();    
+  };  
+
     return (
       <section>
-        <form className={style.form}>
+        <form onSubmit={handleShowUser} className={style.form}>
           <input
             type="text"
             placeholder="Digita um nome de usuário"
             className={style.input}
+            value={user}
+            onChange = {(e) => setUser(e.target.value)}
           />
           <button type="submit" className={style.button}>
             Pesquisar
--- components/userInfo/index.tsx
+++ components/userInfo/index.tsx
@@ -1,24 +1,51 @@
 import style from "../../sass/main.module.scss";
-import UserFollow from "../userFollow";
 import Image from "next/image";
+import useFetch from "../../hook/useFetch";
 
 function UserInfo() {
+  /* const { data, mutate } = useFetch(storage);
+
+
+  if (!data) {
     return (
-      <section className={style.userInfo}>
-        <picture className={style.bgTop}>
-          <Image src="/images/space1.png" width="400px" height="250px" />
-        </picture>
-        <picture className={style.avatar}>
-          <Image src="/images/avatar.jpg" width="150px" height="150px" />
-        </picture>
-        <div className={style.userData}>
-          <h6>Rafael Couto Silva</h6>
-          <p>rcs</p>
-          <p>Sou apaixonado por frontend.</p>
+      <section className={style.noData}>
+        <Image src="/images/reload.svg" width="300px" height="300px"  className={style.realod}/>
+        <div className={style.message}>
+          <span>Carregando, por favor aguarde.</span>
         </div>
-        <UserFollow />
       </section>
     );
+  } 
+
+  return (
+    <section className={style.userInfo}>
+      <picture className={style.bgTop}>
+        <Image src="/images/space1.png" width="400px" height="250px" />
+      </picture>
+      <picture className={style.avatar}>
+        <img src={data.avatar_url} width="150px" height="150px" />
+      </picture>
+      <div className={style.userData}>
+        <h6>{data.name}</h6>
+        <p>{data.location}</p>
+        <p>{data.bio}.</p>
+      </div>
+      <div className={style.boxFollow}>
+        <div>
+          <h6>Repositórios</h6>
+          <p>{data.public_repos}</p>
+        </div>
+        <div>
+          <h6>Seguidores</h6>
+          <p>{data.followers}</p>
+        </div>
+        <div>
+          <h6>Seguindo</h6>
+          <p>{data.following}</p>
+        </div>
+      </div>
+    </section>
+  );*/
 }
 
 export default UserInfo;
