787ae3903e4efa6e8ec883b4f85352d8709dee96
Rafael Couto <rafaelcsilva016@outlook.com>
2021-02-23 20:17:57 -0300

    update project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
 import Layout from "../components/layout";
 import Form from "../components/form";
 import UserInfo from "../components/userInfo";
+import Head from "next/head";
 
 function Home() {
   return (
     <Layout>
+      <Head>
+        <link
+          rel="shortcut icon"
+          href="/images/favicon.ico"
+          type="image/x-icon"
+        />
+        <title>Home | Profile Card</title>
+      </Head>
       <Form />
       <UserInfo />
     </Layout>
